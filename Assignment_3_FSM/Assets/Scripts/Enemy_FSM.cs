13299b22ff19cbe521f77e6f53c20ca792d4d00b
haris-584 <hariskhattak584@gmail.com>
2020-06-07 17:23:28 +0500

    Update Enemy_FSM.cs

--- Assignment_3_FSM/Assets/Scripts/Enemy_FSM.cs
+++ Assignment_3_FSM/Assets/Scripts/Enemy_FSM.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class Enemy_FSM : MonoBehaviour
 {
-    public enum ENEMY_STATE { MOVING, ChASING , FIRING , DEAD , GAMEOVER };
+    public enum ENEMY_STATE { PATROL, CHASE, ATTACK };
     [SerializeField]
     private ENEMY_STATE currentState;
     public ENEMY_STATE CurrentState
@@ -22,20 +22,14 @@ public class Enemy_FSM : MonoBehaviour
             StopAllCoroutines();
             switch (currentState)
             {
-                case ENEMY_STATE.MOVING:
-                    StartCoroutine(EnemyMoving());
+                case ENEMY_STATE.PATROL:
+                    StartCoroutine(EnemyPatrol());
                     break;
-                case ENEMY_STATE.CHASING:
-                    StartCoroutine(EnemyChasing());
+                case ENEMY_STATE.CHASE:
+                    StartCoroutine(EnemyChase());
                     break;
-                case ENEMY_STATE.FIRING:
-                    StartCoroutine(EnemyFiring());
-                    break;
-                case ENEMY_STATE.DEAD:
+                case ENEMY_STATE.ATTACK:
                     StartCoroutine(EnemyAttack());
-                    break;
-                case ENEMY_STATE.GAMEOVER:
-                    StartCoroutine(EnemyGameover());
                     break;
             }
 
@@ -44,46 +38,98 @@ public class Enemy_FSM : MonoBehaviour
 
     private CheckMyVision checkMyVision;
     private NavMeshAgent agent = null;
-   // public Transform playerTransform = null;
-   // private Transform patrolDestination = null;
-   // public float maxDamage = 10f;
+    public Transform playerTransform = null;
+    private Transform patrolDestination = null;
+   // private Health playerHealth = null;
+    public float maxDamage = 10f;
     private void Awake()
     {
         checkMyVision = GetComponent<CheckMyVision>();
         agent = GetComponent<NavMeshAgent>();
-       
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+        //playerTransform = playerHealth.GetComponent<Transform>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject[] destinations = GameObject.FindGameObjectsWithTag("Dest");
-   //     patrolDestination = destinations[Random.Range(0, destinations.Length)].GetComponent<Transform>();
-        CurrentState = ENEMY_STATE.MOVING;
+        patrolDestination = destinations[Random.Range(0, destinations.Length)].GetComponent<Transform>();
+        CurrentState = ENEMY_STATE.PATROL;
     }
 
-    public IEnumerator EnemyMoving()
+    public IEnumerator EnemyPatrol()
     {
+        while (currentState == ENEMY_STATE.PATROL)
+        {
+            checkMyVision.sensitivity = CheckMyVision.enmSensitivity.HIGH;
+            agent.isStopped = false;
+            agent.SetDestination(patrolDestination.position);
+            while (agent.pathPending)
+            {
+                yield return null;
+            }
+
+            if (checkMyVision.targetInSight)
+            {
+                agent.isStopped = true;
+                CurrentState = ENEMY_STATE.CHASE;
+                yield break;
+            }
+            yield return null;
+        }
+
+    }
+    public IEnumerator EnemyChase()
+    {
+        while (currentState == ENEMY_STATE.CHASE)
+        {
+            checkMyVision.sensitivity = CheckMyVision.enmSensitivity.LOW;
+            agent.isStopped = false;
+            agent.SetDestination(checkMyVision.lastKnownSighting);
+            while (agent.pathPending)
+            {
+                yield return null;
+            }
+            Debug.Log(agent.stoppingDistance);
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                agent.isStopped = true;
+                if (!checkMyVision.targetInSight)
+                {
+                    CurrentState = ENEMY_STATE.PATROL;
+                }
+                else
+                {
+                    CurrentState = ENEMY_STATE.ATTACK;
+                }
+                yield break;
+            }
+            yield return null;
+        }
         yield break;
     }
-    public IEnumerator EnemyChasing()
+    public IEnumerator EnemyAttack()
     {
+        while (currentState == ENEMY_STATE.ATTACK)
+        {
+            Debug.Log("test");
+            agent.isStopped = false;
+            agent.SetDestination(playerTransform.position);
+            while (agent.pathPending)
+                yield return null;
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                CurrentState = ENEMY_STATE.CHASE;
+            }
+            else
+            {
+                playerHealth.HealthPoints -= maxDamage * Time.deltaTime;
+            }
+            yield return null;
+        }
         yield break;
     }
-    
-    public IEnumerator EnemyFiring()
-    {
-        yield break;
-    }
-    public IEnumerator EnemyDead()
-    {
-        yield break;
-    }
-    public IEnumerator EnemyGameover()
-    {
-        yield break;
-    }
-     
     // Update is called once per frame
     void Update()
     {
